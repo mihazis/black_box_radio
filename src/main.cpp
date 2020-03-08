2864dc48b4fb313c6989d77ddd3483b548e7d428
nikitinms <ms.nikitin@tensor.ru>
2020-03-09 00:27:01 +0300

    education1

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,10 @@
 #include <Arduino.h>
 #include <Arduino_ST7789.h>
 #include <GyverEncoder.h>
-#include "rainbow.h"
-#include "tensor.h"
-#include "its_all.h"
 #include <FastLED.h>
 #include <WiFi.h>
-
 #include <Adafruit_GFX.h>
 #include <Fonts/FreeMonoBoldOblique12pt7b.h>
-
 #include <Fonts/FreeMonoOblique24pt7b.h>
 
 //в определениях указываем первоначально все используемые пины
@@ -37,20 +32,24 @@
 #define LED_DATA  23
 #define NUM_LEDS 8
 //таймеры
-#define PERIOD_1 600000    // перерыв между включением диодов (мс)
-#define PERIOD_2 1000      // время работы диодов
-#define PERIOD_3 66611111
+#define PERIOD_1 10000    // перерыв между включением диодов (мс)
+#define PERIOD_2 100000      // время работы диодов
+#define PERIOD_3 600000
+#define PERIOD_4 1000
 
 //инициализируем оборудование
 Encoder enc1(ENC_CLK, ENC_DT, ENC_SW);
 Arduino_ST7789 tft = Arduino_ST7789(TFT_DC, TFT_RST, TFT_MOSI, TFT_SCLK); //for display without CS pin
+
 //определяем массив led
 CRGB leds[NUM_LEDS]; 
+
 //константы для вайфая
 const char *SSID = "Tensor"; // имя точки
 const char *PASSWORD = "87654321"; // пароль
+
 //для таймеров
-unsigned long timer_1, timer_2, timer_3;
+unsigned long timer_1, timer_2, timer_3, timer_4;
 
 const float p = 3.1415926;
 
@@ -67,6 +66,43 @@ void setup(void) {
   FastLED.setBrightness(100);
 
   WiFi.begin(SSID, PASSWORD);
+  timer_1 = millis();
+  timer_2 = millis();
+  timer_3 = millis();
+  timer_4 = millis();
+  byte bright = 0;
+  
+  /*
+  tft.drawLine(0, 1, 240, 1, RED); //линия
+  tft.drawLine(0, 11, 240, 11, WHITE); //линия
+  tft.drawLine(0, 21, 240, 21, WHITE); //линия
+  tft.drawLine(0, 31, 240, 31, WHITE); //линия
+  tft.drawLine(0, 41, 240, 41, WHITE); //линия
+  tft.drawLine(0, 51, 240, 51, RED); //линия
+  tft.drawLine(0, 61, 240, 61, WHITE); //линия
+  tft.drawLine(0, 71, 240, 71, WHITE); //линия
+  tft.drawLine(0, 81, 240, 81, WHITE); //линия
+  tft.drawLine(0, 91, 240, 91, WHITE); //линия
+  tft.drawLine(0, 101, 240, 101, GREEN); //линия
+  tft.drawLine(0, 111, 240, 111, WHITE); //линия
+  tft.drawLine(0, 121, 240, 121, WHITE); //линия
+  tft.drawLine(0, 131, 240, 131, WHITE); //линия
+  tft.drawLine(0, 141, 240, 141, WHITE); //линия
+  tft.drawLine(0, 151, 240, 151, RED); //линия
+  tft.drawLine(0, 161, 240, 161, WHITE); //линия
+  tft.drawLine(0, 171, 240, 171, WHITE); //линия
+  tft.drawLine(0, 181, 240, 181, WHITE); //линия
+  tft.drawLine(0, 191, 240, 191, WHITE); //линия
+  tft.drawLine(0, 201, 240, 201, GREEN); //линия
+  tft.drawLine(0, 211, 240, 211, WHITE); //линия
+  tft.drawLine(0, 221, 240, 221, WHITE); //линия
+  tft.drawLine(0, 231, 240, 231, WHITE); //линия
+  tft.drawCircle(10, 10, 5, BLUE);
+  tft.drawCircle(30, 30, 30, WHITE);
+  tft.drawCircle(50, 50, 50, WHITE);
+  tft.fillCircle(100, 100, 50, GREEN);
+  */
+
   }
 
 void testdrawtext(char *text, uint16_t color) {
@@ -76,45 +112,59 @@ void testdrawtext(char *text, uint16_t color) {
   tft.setFont(&FreeMonoOblique24pt7b);
   tft.print(text);
 }
+void testdrawtext2(char *text, uint16_t color) {
+  tft.setCursor(25, 150);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.setFont(&FreeMonoOblique24pt7b);
+  tft.print(text);
+}
+void textdownright(char *text, uint16_t color) {
+  tft.setCursor(120, 200);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.setFont(&FreeMonoOblique24pt7b);
+  tft.print(text);
+}
 
 void loop() {
 
   enc1.tick(); // обязательная функция отработки. Должна постоянно опрашиваться
-
+  byte bright;
+  
   //===============Первый таймер=============================
 
   if (millis() - timer_1 > PERIOD_1) {
     timer_1 = millis();                   // сброс таймера
-    fill_solid(leds, NUM_LEDS, CRGB::Aquamarine);
-    FastLED.show();
     testdrawtext("ZABBIX", RED);
+    //print1 = 
+    //testdrawtext2(timer_1, RED);
     
   }
-  if (millis() - timer_1 > PERIOD_2) {
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
-    FastLED.show();
+  if (millis() - timer_2 > PERIOD_2) {
+    timer_2 = millis();
     tft.fillScreen(BLACK);
   }
   //=========================================================
 
-      
-  //===============Второй таймер=============================
-  if (millis() - timer_2 > PERIOD_2) {
-    timer_2 = millis();
-    //tft.fillScreen(BLACK);
-    
-  }
-  //=========================================================
+  
+  //===============Второй таймер для leds====================
+  if (millis() - timer_3 > PERIOD_3) {  //заходим внутрь, если прошло три секунды (PERIOD_2) со старта всего loop
+    timer_3 = millis();                //приравниваем timer_2 к текущиму времени, после чего
+    fill_solid(leds, NUM_LEDS, CRGB::Aquamarine);
+    FastLED.show();
+    }
 
-
-  //===============Третий таймер=============================
-  if (millis() - timer_3 > PERIOD_3) {
-    timer_3 = millis();
-        
+  if (millis() - timer_4 > PERIOD_4) {
+    timer_4 = millis();
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+    }    
   }
   //=========================================================
   
-}
+
+
 
 
 
